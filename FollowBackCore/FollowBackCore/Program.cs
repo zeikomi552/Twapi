3991db4ddd46c69c63ccb7f5cb7a53357d9b783e
zeikomi552 <68983834+zeikomi552@users.noreply.github.com>
2021-12-09 06:06:29 +0900

    コマンド体系を整備

--- FollowBackCore/FollowBackCore/Program.cs
+++ FollowBackCore/FollowBackCore/Program.cs
@@ -1,81 +1,161 @@
-﻿using FollowBackCore.Twitter;
+﻿using ClosedXML.Excel;
+using FollowBackCore.Twitter;
 using FollowBackCore.Utilities;
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Text;
 
 namespace FollowBackCore
 {
     class Program
     {
-        static bool GetKeys(string[] args, ref TwitterKeys keys, ref string command)
+        static Dictionary<string, string> _Args = new Dictionary<string, string>();
+        static TwitterKeys _Keys = new TwitterKeys();
+        static string _Action = string.Empty;
+        static void SetArgs(string[] args)
         {
-            int min_args = 5;
-
-            if (args.Length <= min_args)
+            for (int i = 0; i < args.Length; i++)
             {
-                return false;
+                switch (args[i].ToLower())
+                {
+                    case "-tw":
+                    case "-twd":
+                    case "-tws":
+                    case "-frc":
+                    case "-frd":
+                    case "-fvc":
+                    case "-fvd":
+                        {
+                            _Args.Add("action", args.Length > i ? args[i] : string.Empty);
+                            break;
+                        }
+                    case "-msg":
+                        {
+                            _Args.Add("message", args.Length > i ? args[i] : string.Empty);
+                            break;
+                        }
+                    case "-scn":
+                        {
+                            i++;
+                            _Args.Add("screen_name", args.Length > i ? args[i] : string.Empty);
+                            break;
+                        }
+                    case "-kwd":
+                        {
+                            i++;
+                            _Args.Add("search_keyword", args.Length > i ? args[i] : string.Empty);
+                            break;
+                        }
+                    case "-f":
+                        {
+                            i++;
+                            _Args.Add("file_path", args.Length > i ? args[i] : string.Empty);
+                            break;
+                        }
+                    case "-ckey":
+                        {
+                            i++;
+                            _Keys.ConsumerKey = args.Length > i ? args[i] : string.Empty;
+                            break;
+                        }
+                    case "-cscr":
+                        {
+                            i++;
+                            _Keys.ConsumerSecretKey = args.Length > i ? args[i] : string.Empty;
+                            break;
+                        }
+                    case "-atkn":
+                        {
+                            i++;
+                            _Keys.AccessToken = args.Length > i ? args[i] : string.Empty;
+                            break;
+                        }
+                    case "-ascr":
+                        {
+                            i++;
+                            _Keys.AccessSecret = args.Length > i ? args[i] : string.Empty;
+                            break;
+                        }
+
+                }
+
             }
-
-            command = args[0];
-
-            keys = new TwitterKeys();
-            keys.ConsumerKey = args[1];
-            keys.ConsumerSecretKey = args[2];
-            keys.AccessToken = args[3];
-            keys.AccessSecret = args[4];
-
-            return true;
-        }
-
-        static bool GetArg(string[] args, int index, ref string arg)
-        {
-            if (args.Length > index)
-            {
-                arg = args[index];
-                return true;
-            }
-            return false;
         }
 
         static void Main(string[] args)
         {
-            TwitterKeys keys = new TwitterKeys();
-            string command = string.Empty;
+            // 引数のセット
+            SetArgs(args);
+            var action = _Args["action"];
 
-            if (GetKeys(args, ref keys, ref command))
+            switch (action)
             {
-                switch (command)
-                {
-                    case "tweet":
+                case "-h":
+                    {
+                        Console.WriteLine("\tFollowBackCore.exe -h   ...ヘルプ");
+                        Console.WriteLine("\tFollowBackCore.exe -tw    ...ヘルプ");
+                        break;
+                    }
+                case "-tw":
+                    {
+                        var message = _Args["message"];
+                        TwitterAPI.Tweet(_Keys, message.Replace("\\r\\n", "\r\n"));
+                        break;
+                    }
+                case "-cfr":
+                    {
+                        string screen_name = _Args["screen_name"];
+                        TwitterAPI.CreateFollow(_Keys, screen_name);
+                        break;
+                    }
+                case "-dfr":
+                    {
+                        string screen_name = _Args["screen_name"];
+                        TwitterAPI.BreakFollow(_Keys, screen_name);
+                        break;
+                    }
+                case "-tws":
+                    {
+                        string search_key = _Args["search_keyword"];
+                        var ret = TwitterAPI.TweetSearch(_Keys, search_key);
+                        int index = 1;
+
+                        // Excelファイルを作る
+                        using (var workbook = new XLWorkbook())
                         {
-                            string message = string.Empty;
-                            if (GetArg(args, 5, ref message))
+                            var worksheet = workbook.Worksheets.Add("result");
+                            // セルに値や数式をセット
+                            worksheet.Cell($"A{index}").Value = "tweet.ID";
+                            worksheet.Cell($"B{index}").Value = "user.Id";
+                            worksheet.Cell($"C{index}").Value = "screen_name";
+                            worksheet.Cell($"D{index}").Value = "text";
+                            worksheet.Cell($"E{index}").Value = "FavoriteCount";
+                            worksheet.Cell($"F{index}").Value = "user.FriendsCount";
+                            worksheet.Cell($"G{index}").Value = "user.FollowerCount";
+                            worksheet.Cell($"H{index}").Value = "CreateAt";
+
+                            foreach (var tw in ret)
                             {
-                                TwitterAPI.Tweet(keys, message.Replace("\\r\\n", "\r\n"));
-                            }
-                            break;
-                        }
-                    case "createfriend":
-                        {
-                            string screen_name = string.Empty;
-                            if (GetArg(args, 5, ref screen_name))
-                            {
-                                TwitterAPI.CreateFollow(keys, screen_name);
+                                index++;
+                                worksheet.Cell($"A{index}").Value = tw.Id;
+                                worksheet.Cell($"B{index}").Value = tw.User.Id;
+                                worksheet.Cell($"C{index}").Value = tw.User.ScreenName;
+                                worksheet.Cell($"D{index}").Value = tw.Text;
+                                worksheet.Cell($"E{index}").Value = tw.FavoriteCount;
+                                worksheet.Cell($"F{index}").Value = tw.User.FriendsCount;
+                                worksheet.Cell($"G{index}").Value = tw.User.FollowersCount;
+                                worksheet.Cell($"H{index}").Value = tw.CreatedAt.LocalDateTime;
                             }
 
-                            break;
+                            string file_path = _Args["file_path"];
+                            // ワークブックを保存する
+                            workbook.SaveAs(file_path);
                         }
-                    case "breakfriend":
-                        {
-                            string screen_name = string.Empty;
-                            if (GetArg(args, 5, ref screen_name))
-                            {
-                                TwitterAPI.BreakFollow(keys, screen_name);
-                            }
+                        //}
 
-                            break;
-                        }
-                }
+                        break;
+                    }
             }
         }
 
