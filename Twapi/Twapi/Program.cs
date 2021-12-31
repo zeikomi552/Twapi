e22ef927f879845c956d08cae8f3e185063fb86f
zeikomi552 <68983834+zeikomi552@users.noreply.github.com>
2022-01-01 08:01:11 +0900

    出力内容の誤りを修正

--- Twapi/Twapi/Program.cs
+++ Twapi/Twapi/Program.cs
@@ -37,10 +37,10 @@ namespace Twapi
                 StringBuilder msg = new StringBuilder();
                 foreach (var arg in args) msg.Append(arg + " ");
 
-                Logger.Error("twapi " + e.Message);
+                Logger.Error("twapi " + msg.ToString());
                 Logger.Error(e.Message);
 
-                Console.WriteLine("twapi " + e.Message);
+                Console.WriteLine("twapi " + msg.ToString());
                 Console.WriteLine(e.Message);
                 return -1;
             }
